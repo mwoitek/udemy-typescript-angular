6a996f19bd5301edc538660ddba6b61b579e0ac4
Marcio Woitek <woitek@usp.br>
2021-07-08 15:33:48 -0300

    Código da aula 19

--- typescript/app2.ts
+++ typescript/app2.ts
@@ -20,3 +20,20 @@ class Carro {
         return this.velocidade;
     }
 }
+
+class Concessionaria {
+    private endereco: string;
+    private listaDeCarros: any;
+
+    constructor(endereco: string) {
+        this.endereco = endereco;
+    }
+
+    public fornecerEndereco(): string {
+        return this.endereco;
+    }
+
+    public mostrarListaDeCarros(): any {
+        return this.listaDeCarros;
+    }
+}
--- typescript/out/app2.js
+++ typescript/out/app2.js
@@ -16,3 +16,15 @@ var Carro = /** @class */ (function () {
     };
     return Carro;
 }());
+var Concessionaria = /** @class */ (function () {
+    function Concessionaria(endereco) {
+        this.endereco = endereco;
+    }
+    Concessionaria.prototype.fornecerEndereco = function () {
+        return this.endereco;
+    };
+    Concessionaria.prototype.mostrarListaDeCarros = function () {
+        return this.listaDeCarros;
+    };
+    return Concessionaria;
+}());
