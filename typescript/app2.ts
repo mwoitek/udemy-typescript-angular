2676a626c19533c654eabfd96c579ca100654568
Marcio Woitek <woitek@usp.br>
2021-07-08 16:06:07 -0300

    Código da aula 20

--- typescript/app2.ts
+++ typescript/app2.ts
@@ -37,3 +37,30 @@ class Concessionaria {
         return this.listaDeCarros;
     }
 }
+
+class Pessoa {
+    private nome: string;
+    private carroPreferido: string;
+    private carro: any;
+
+    constructor(nome: string, carroPreferido: string) {
+        this.nome = nome;
+        this.carroPreferido = carroPreferido;
+    }
+
+    public dizerNome(): string {
+        return this.nome;
+    }
+
+    public dizerCarroPreferido(): string {
+        return this.carroPreferido;
+    }
+
+    public comprarCarro(carro: any): void {
+        this.carro = carro;
+    }
+
+    public dizerCarroQueTem(): any {
+        return this.carro;
+    }
+}
--- typescript/out/app2.js
+++ typescript/out/app2.js
@@ -28,3 +28,22 @@ var Concessionaria = /** @class */ (function () {
     };
     return Concessionaria;
 }());
+var Pessoa = /** @class */ (function () {
+    function Pessoa(nome, carroPreferido) {
+        this.nome = nome;
+        this.carroPreferido = carroPreferido;
+    }
+    Pessoa.prototype.dizerNome = function () {
+        return this.nome;
+    };
+    Pessoa.prototype.dizerCarroPreferido = function () {
+        return this.carroPreferido;
+    };
+    Pessoa.prototype.comprarCarro = function (carro) {
+        this.carro = carro;
+    };
+    Pessoa.prototype.dizerCarroQueTem = function () {
+        return this.carro;
+    };
+    return Pessoa;
+}());
