21cd5a028103ffc03b5860465e73c11fb2a7eeb2
Marcio Woitek <woitek@usp.br>
2021-07-11 09:22:41 -0300

    Código até a aula 39

---
+++ angular/projetos/app1/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+
+@NgModule({
+  declarations: [AppComponent, TopoComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ angular/projetos/app1/src/app/topo/topo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topo',
+  templateUrl: './topo.component.html',
+  // selector: 'app-topo',
+})
+export class TopoComponent {}
