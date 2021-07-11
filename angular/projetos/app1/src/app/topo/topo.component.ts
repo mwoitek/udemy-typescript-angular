5883811c86f6cea251000803998a66da1993de8e
Marcio Woitek <woitek@usp.br>
2021-07-11 09:42:56 -0300

    Código até a aula 41

--- angular/projetos/app1/src/app/topo/topo.component.ts
+++ angular/projetos/app1/src/app/topo/topo.component.ts
@@ -3,6 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
-  // selector: 'app-topo',
+  styleUrls: ['./topo.component.css'],
+  // template: '<p>Essa é a componente Topo.</p>',
+  // styles: [
+  //   `
+  //     .exemplo {
+  //       color: red;
+  //     }
+  //   `,
+  // ],
 })
 export class TopoComponent {}
