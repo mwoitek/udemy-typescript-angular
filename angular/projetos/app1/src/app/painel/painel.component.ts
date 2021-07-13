47f35c4fc9998c61f8b0a00c6fc35e6dc0c17cdd
Marcio Woitek <woitek@usp.br>
2021-07-12 23:29:39 -0300

    Código até a aula 67

--- angular/projetos/app1/src/app/painel/painel.component.ts
+++ angular/projetos/app1/src/app/painel/painel.component.ts
@@ -11,10 +11,13 @@ import { FRASES } from './frases-mock';
 export class PainelComponent implements OnInit {
   public frases: Frase[] = FRASES;
   public instrucao = 'Traduza a frase:';
+  public progresso = 0;
   public resposta = '';
+  public rodada = 0;
+  public rodadaFrase: Frase = new Frase('', '');
 
   constructor() {
-    //
+    this.atualizaRodada();
   }
 
   ngOnInit(): void {
@@ -23,6 +26,24 @@ export class PainelComponent implements OnInit {
 
   public atualizaResposta(resposta: Event): void {
     this.resposta = (<HTMLInputElement>resposta.target).value;
-    console.log(this.resposta);
+  }
+
+  public atualizaRodada(): void {
+    // Trocar frase da rodada
+    this.rodadaFrase = this.frases[this.rodada];
+    // Limpar a resposta
+    this.resposta = '';
+  }
+
+  public verificarResposta(): void {
+    if (this.rodadaFrase.frasePtBr === this.resposta) {
+      alert('A tradução está correta!');
+      this.rodada++;
+      // Atualizar o progresso
+      this.progresso += 100 / this.frases.length;
+      this.atualizaRodada();
+    } else {
+      alert('A tradução está errada!');
+    }
   }
 }
--- angular/projetos/app1/src/app/progresso/progresso.component.ts
+++ angular/projetos/app1/src/app/progresso/progresso.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-progresso',
   templateUrl: './progresso.component.html',
-  styleUrls: ['./progresso.component.css']
+  styleUrls: ['./progresso.component.css'],
 })
 export class ProgressoComponent implements OnInit {
+  @Input() public progresso = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    //
   }
 
+  ngOnInit(): void {
+    //
+  }
 }
